6381251e012e305293504eda1a0d8fe8b8530917
Rolex <101451384+r0l3x007@users.noreply.github.com>
2023-09-13 17:09:15 +0300

    Added boilerplate code and 2D Array generator

---
+++ script.js
@@ -0,0 +1,25 @@
+let gameBoard = {
+}
+
+
+const rows = 3;
+const columns = 3;
+
+const gameboard = new Array(rows);
+
+for(let i =  0; i < rows; i ++){
+    gameboard[i] = new Array(columns)
+}
+
+
+gameBoard.emptyBoard = gameboard;
+
+
+
+const playerFactory = (playerName, playerMarker) =>{
+
+}
+
+
+let gameContainer = document.querySelector(`container`);
+
